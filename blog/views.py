ddddbee7a25d178f16efc642a2b0cdd897d80419
Stranix <Phantom2525@gmail.com>
2023-05-02 15:27:38 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -30,7 +30,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -38,7 +38,7 @@ def serialize_post(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts__count,
     }
 
 
@@ -79,7 +79,7 @@ def post_detail(request, slug):
 
     likes = post.likes.all()
 
-    related_tags = post.tags.all()
+    related_tags = post.tags.annotate(Count('posts'))
 
     serialized_post = {
         'title': post.title,
