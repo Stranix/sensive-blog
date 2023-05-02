7d199817e4671df5d18229f500faf231ea3c1715
Stranix <Phantom2525@gmail.com>
2023-05-02 15:38:03 +0300

    prefetch posts count for tags

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
@@ -16,7 +16,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(Count('posts'))],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -45,12 +45,11 @@ def serialize_tag(tag):
 def index(request):
     most_popular_posts = Post.objects.popular() \
                              .prefetch_related('author')[:5] \
-                             .prefetch_related('tags') \
+                             .prefetch_related(Prefetch('tags', queryset=Tag.objects.all().annotate(Count('posts')))) \
                              .fetch_with_comments_count()
-
     most_fresh_posts = Post.objects.annotate(comments_count=Count('comments')) \
                            .prefetch_related('author') \
-                           .prefetch_related('tags') \
+                           .prefetch_related(Prefetch('tags', queryset=Tag.objects.all().annotate(Count('posts')))) \
                            .order_by('-published_at')[:5]
 
     most_popular_tags = Tag.objects.popular()[:5]
@@ -101,7 +100,7 @@ def post_detail(request, slug):
         'post': serialized_post,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
     }
     return render(request, 'post-details.html', context)
